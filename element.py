3d85305c6ec29af59f46ab520f1feacd5188c873
zaxii <38310130+zaxii@users.noreply.github.com>
2020-04-13 00:14:55 +0800

    Add files via upload
    reform code in class/ fix the calculation of value/ still have problems

---
+++ element.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class Element(Enum):
+    PASS = 0
+    BLOCK = 1
+    TRAP = 2
+    TERMINAL = 3
---
+++ main.py
@@ -0,0 +1,8 @@
+from AIMazeFindPath import AIMazeFindPath
+
+
+my_instance = AIMazeFindPath()
+my_instance.run()
+my_instance.show_result()
+
+
